3bc301288bc14e9f4c212c3db3f40be5769a6b80
Doug DeBold <ddebold56@gmail.com>
2016-03-23 14:57:52 -0500

    Add back slack functionality

--- server.js
+++ server.js
@@ -22,63 +22,63 @@ var server = app.listen( app.get('port'), function() {
   console.log('Express listening at http://%s:%s', server.address().address, server.address().port);
 });
 
-// // Express app configs
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
+// Express app configs
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// // Allow crossdomain
-// app.use(function(req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//   res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-//   next();
-// });
+// Allow crossdomain
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  next();
+});
 
-// // Query messages
-// app.get('/channel/:id/messages', function(req, res, next) {
-//   if( req.params.id ) {
-//     slack.api('channels.history', {
-//       channel: req.params.id,
-//       oldest: req.query.oldest
-//     }, function(err, response) {
-//       res.send(response);
-//     });
-//   } else {
-//     res.send({ error : 'Data missing'});
-//   }
-// });
+// Query messages
+app.get('/channel/:id/messages', function(req, res, next) {
+  if( req.params.id ) {
+    slack.api('channels.history', {
+      channel: req.params.id,
+      oldest: req.query.oldest
+    }, function(err, response) {
+      res.send(response);
+    });
+  } else {
+    res.send({ error : 'Data missing'});
+  }
+});
 
-// // Create channel
-// app.post('/channels', function(req, res, next) {
-//   slack.api('channels.create', {
-//     name: req.body.channel
-//   }, function(err, response) {
-//     res.send(response);
+// Create channel
+app.post('/channels', function(req, res, next) {
+  slack.api('channels.create', {
+    name: req.body.channel
+  }, function(err, response) {
+    res.send(response);
 
-//     // Invite extra users
-//     supportUserIds.forEach(function(userid) {
-//       slack.api('channels.invite', {
-//         channel: response.channel.id,
-//         user: userid
-//       });
-//     });
-//   });
-// });
+    // Invite extra users
+    supportUserIds.forEach(function(userid) {
+      slack.api('channels.invite', {
+        channel: response.channel.id,
+        user: userid
+      });
+    });
+  });
+});
 
-// // Send message
-// app.post('/message', function(req, res, next) {
-//   if( req.body.message && req.body.channel ){
-//     slack.api('chat.postMessage', {
-//       text:req.body.message,
-//       channel: req.body.channel,
-//       attachments: req.body.attachments
-//     }, function(err, postMessageResponse){
-//       res.send(postMessageResponse);
-//     });
-//   } else{
-//     res.send({ error : 'Data missing'});
-//   }
-// });
+// Send message
+app.post('/message', function(req, res, next) {
+  if( req.body.message && req.body.channel ){
+    slack.api('chat.postMessage', {
+      text:req.body.message,
+      channel: req.body.channel,
+      attachments: req.body.attachments
+    }, function(err, postMessageResponse){
+      res.send(postMessageResponse);
+    });
+  } else{
+    res.send({ error : 'Data missing'});
+  }
+});
 
 // Test that the server is working.
 app.get('/', function(request, response) {
